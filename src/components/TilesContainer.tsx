9fc3295e4147a3be2221c275e97a58360b516239
Jacob Taylor <yepyepguru@gmail.com>
2025-02-11 11:36:21 +0800

    update random algo

--- src/components/TilesContainer.tsx
+++ src/components/TilesContainer.tsx
@@ -87,7 +87,9 @@ export default function TilesContainer() {
             setTileConfig((prevConfig) => {
                 let shuffled = [...prevConfig];
                 for (let i = shuffled.length - 1; i > 0; i--) {
-                    const j = Math.floor(Math.random() * (i + 1));
+                    const randomArray = new Uint32Array(1);
+                    window.crypto.getRandomValues(randomArray);
+                    const j = randomArray[0] % (i + 1);
                     [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
                 }
                 return shuffled;
